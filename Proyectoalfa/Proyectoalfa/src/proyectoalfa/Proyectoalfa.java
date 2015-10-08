9a6336a88677399f98688079b375c4ebd7b64a7a
julianlinux <julianlinux@julianlinux-X555LA>
2015-10-08 09:33:45 +0200

    Primer commit

---
+++ Proyectoalfa/Proyectoalfa/src/proyectoalfa/Proyectoalfa.java
@@ -0,0 +1,25 @@
+
+package proyectoalfa;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author julianlinux
+ */
+public class Proyectoalfa {
+
+    
+     
+    public static void main(String[] args) {
+          float precio,pagado,total;
+        System.out.println("precio original:");
+        Scanner obx=new Scanner (System.in);
+        precio=obx.nextFloat();
+        System.out.println("Introduce el precio pagado");
+        pagado=obx.nextFloat();
+        total=((precio-pagado)/precio)*100;
+        System.out.println("Descuento="+total+"%");
+    }
+    
+}
